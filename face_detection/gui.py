618f895f79493be279a3295434c1cd78fe977690
Bung1e3 <leshkachovzhik@gmail.com>
2023-11-05 13:30:17 +0100

    "some changes"

--- face_detection/gui.py
+++ face_detection/gui.py
@@ -64,7 +64,7 @@ def show_vid():
         num_faces = bounding_box.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
 
         for (x, y, w, h) in num_faces:
-            cv2.rectangle(frame1, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            cv2.rectangle(frame1, (x, y), (x+w, y+h), (255, 0, 0), 2)
             roi_gray_frame = gray_frame[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
             prediction = emotion_model.predict(cropped_img)
