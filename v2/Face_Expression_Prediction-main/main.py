a868a3242db34ac8f4a8c1b074c4b239e9a7b0f8
Bung1e3 <leshkachovzhik@gmail.com>
2023-12-16 18:09:35 +0100

    update

--- v2/Face_Expression_Prediction-main/main.py
+++ v2/Face_Expression_Prediction-main/main.py
@@ -8,7 +8,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0) # An important note -> to hange the input replace 0 with the video file path you want to add
+        self.video = cv2.VideoCapture(0) 
 
     def __del__(self):
         self.video.release()
